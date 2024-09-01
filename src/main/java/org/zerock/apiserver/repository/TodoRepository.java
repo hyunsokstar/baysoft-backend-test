ab348b791d89081b2fa737628a609f325b84432e
ohhyunsok <terecal@daum.net>
2024-09-01 17:44:16 +0900

    add repository and basic test

--- src/main/java/org/zerock/apiserver/repository/TodoRepository.java
+++ src/main/java/org/zerock/apiserver/repository/TodoRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.apiserver.domain.Todo;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    // 필요한 경우 추가적인 쿼리 메서드를 정의할 수 있습니다.
 }
