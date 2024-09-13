a1f3ddace0234a619180edcfe6d404935febc81e
ohhyunsok <terecal@daum.net>
2024-09-13 21:31:34 +0900

    update for pagination

--- src/main/java/org/zerock/apiserver/dto/PageResponseDTO.java
+++ src/main/java/org/zerock/apiserver/dto/PageResponseDTO.java
@@ -26,15 +26,22 @@ public class PageResponseDTO<E> {
         this.pageRequestDTO = pageRequestDTO;
         this.totalCount = (int)total;
 
-        // 끝 페이지 end
+        // 현재 페이를 기준으로 끝 페이지 계산
         int end = (int) (Math.ceil(pageRequestDTO.getPage() / 10.0)) * 10;
+
+        // end 를 기준으로 첫페이지 계산
         int start = end - 9;
 
+        // 마지막 페이지
+        // 총개수를 기준으로 계산
         int last = (int)(Math.ceil(totalCount/(double)pageRequestDTO.getSize()));
 
-        end = end > last ? last : end;
-
+        end = Math.min(end, last);
+        
+        // 이전 블록으로 넘어 갈 수 있는지 여부
         this.prev = start > 1;
+
+        // 다음 블록으로 넘어 갈 수 있는지 여부
         this.next = (int)totalCount > end * pageRequestDTO.getSize();
 
         // 시작 페이지와 끝 페이지 번호를 이용해 페이지 번호 리스트 만들기
--- src/main/java/org/zerock/apiserver/repository/search/TodoSearchImpl.java
+++ src/main/java/org/zerock/apiserver/repository/search/TodoSearchImpl.java
@@ -21,54 +21,6 @@ public class TodoSearchImpl extends QuerydslRepositorySupport implements TodoSea
         super(Todo.class);  // 엔티티 클래스를 명시적으로 전달
     }
 
-//    @Override
-//    public PageResponseDTO<TodoDTO> search(SearchRequestDTO searchRequestDTO) {
-//        log.info("search with SearchRequestDTO...........");
-//
-//        QTodo todo = QTodo.todo;
-//        JPQLQuery<Todo> query = from(todo);
-//
-//        // 검색 조건 추가
-//        BooleanBuilder builder = new BooleanBuilder();
-//
-//        if (searchRequestDTO.getSearchKeyword() != null && !searchRequestDTO.getSearchKeyword().isEmpty()) {
-//            if ("title".equals(searchRequestDTO.getSearchType())) {
-//                builder.and(todo.title.contains(searchRequestDTO.getSearchKeyword()));
-//            } else if ("content".equals(searchRequestDTO.getSearchType())) {
-//                builder.and(todo.content.contains(searchRequestDTO.getSearchKeyword()));
-//            } else {
-//                // 기본적으로 제목과 내용 모두 검색
-//                builder.and(todo.title.contains(searchRequestDTO.getSearchKeyword())
-//                        .or(todo.content.contains(searchRequestDTO.getSearchKeyword())));
-//            }
-//        }
-//
-//        query.where(builder);
-//
-//        Pageable pageable = PageRequest.of(
-//                searchRequestDTO.getPage() - 1,
-//                searchRequestDTO.getSize(),
-//                Sort.by("tno").descending());
-//
-//        // 페이징 적용
-//        this.getQuerydsl().applyPagination(pageable, query);
-//
-//        // 쿼리 실행 및 결과 반환
-//        List<Todo> list = query.fetch();
-//        long total = query.fetchCount();
-//
-//        // Todo 엔티티를 TodoDTO로 변환
-//        List<TodoDTO> dtoList = list.stream()
-//                .map(this::entityToDto)
-//                .collect(Collectors.toList());
-//
-//        return PageResponseDTO.<TodoDTO>withAll()
-//                .dtoList(dtoList)
-//                .pageRequestDTO(searchRequestDTO)
-//                .total(total)
-//                .build();
-//    }
-
     public PageResponseDTO<TodoDTO> search(SearchRequestDTO searchRequestDTO) {
         log.info("search with SearchRequestDTO...........");
 
